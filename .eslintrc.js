22f8f6de034fc6ec475f1ec5769d7086a996ef27
Egor Ignatyev <egoriggy@gmail.com>
2022-11-30 15:03:08 +0100

    Add a ts test base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:react/recommended', 'standard-with-typescript',],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react',],
-  rules: {
-    'comma-dangle': ['error', 'always',],
-    semi: ['error', 'always',],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['plugin:react/recommended', 'standard-with-typescript'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'comma-dangle': [
+            'error',
+            { arrays: 'always-multiline', objects: 'always-multiline' },
+        ],
+        semi: ['error', 'always'],
+        indent: ['error', 4],
+    },
 };
